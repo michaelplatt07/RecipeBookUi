7165df778a1ae9cb308818d6825eefd4610fb206
plattmetrica <michael@Sakura.com>
2018-09-23 23:14:07 -0400

    MAP : Adding functionality for including a blank quantity for ingredients as people may want to upload something like 4 tomatoes for the ingredients.

--- utils/formatter.js
+++ utils/formatter.js
@@ -1,18 +1,17 @@
 exports.formatTime = (unformattedTime) => {
     const formattedTime = {}
-    formattedTime.hours = unformattedTime / 60
-    formattedTime.minutes = unformattedTime % 60
+    formattedTime.hours = Math.round(unformattedTime / 60)
+    formattedTime.minutes = Math.round(unformattedTime % 60)
     return formattedTime
 }
 
 exports.formatIngredients = (ingredientList, quantityList, unitList) => {
     let ingredients = []
-
     if (ingredientList.constructor === Array && ingredientList.length > 1) {
 	ingredientList.forEach((ingredient, index) => {
 	    let ingredientJSON = {}
 	    ingredientJSON.quantity = quantityList[index]
-	    ingredientJSON.measurement = unitList[index]
+	    ingredientJSON.measurement = unitList[index] === 'undefined' ? '' : unitList[index]
 	    ingredientJSON.text_friendly_name = ingredientList[index]
 	    ingredients.push(ingredientJSON)
 	})
@@ -20,7 +19,7 @@ exports.formatIngredients = (ingredientList, quantityList, unitList) => {
     else {
 	let ingredientJSON = {}
 	ingredientJSON.quantity = quantityList
-	ingredientJSON.measurement = unitList
+	ingredientJSON.measurement = typeof unitList === 'undefined' ? '' : unitList
 	ingredientJSON.text_friendly_name = ingredientList
 	ingredients.push(ingredientJSON)
     }
